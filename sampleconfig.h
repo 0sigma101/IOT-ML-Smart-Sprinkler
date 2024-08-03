3676ca46b7665b5f0c0421c7ca5eb638d530282a
Pallav Gupta <95297658+0sigma101@users.noreply.github.com>
2024-08-03 20:34:07 +0530

    sampleconfig to be referred

---
+++ sampleconfig.h
@@ -0,0 +1,13 @@
+// rename as config.h and use the below code
+// #ifndef CONFIG_H
+// #define CONFIG_H
+
+// #define WIFI_SSID
+// #define WIFI_PASSWORD
+// #define CHANNEL_NUMBER
+// #define WRITE_API_KEY
+// #define BLYNK_TEMPLATE_ID
+// #define BLYNK_TEMPLATE_NAME
+// #define BLYNK_AUTH_TOKEN
+
+// #endif // CONFIG_H
